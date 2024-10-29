cebd56b5687b25bac2e25493536f3dab956bad9c
jjmuesesq <jjmuesesq@unal.edu.co>
2024-10-29 15:32:53 -0500

    :sparkles: Metodos de ayuda para los errores

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -19,6 +19,24 @@ export class BasicPageComponent implements OnInit {
     //this.myForm.reset( rtx5090 );
   }
 
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched;
+  }
+
+  getFieldError( field:string ): string | null {
+    if(!this.myForm.controls[field]) return null;
+    const errors = this.myForm.controls[field].errors || {};
+    for (const key of Object.keys(errors)) {
+      switch ( key ) {
+        case 'required':
+          return 'Este campo es requerido.';
+        case 'minlength':
+          return `Minimo ${ errors['minlength'].requiredLength } characters.`;
+      }
+    }
+    return null;
+  }
+
   /*public myForm: FormGroup = new FormGroup({
     name: new FormControl(''),
     price: new FormControl(0),

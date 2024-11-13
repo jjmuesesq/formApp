a0dc820d712fa95520255612918b8fb2a2f2f8fc
jjmuesesq <jjmuesesq@unal.edu.co>
2024-11-12 23:34:43 -0500

    :sparkles: Formulario de registro

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class RegisterPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [ Validators.required ]],
+    email: ['', [ Validators.required ]],
+    username: ['', [ Validators.required ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    password2: ['', [ Validators.required ]],
+  });
+
+  constructor ( private fb: FormBuilder ) {}
+
+  isValidField() {
+    //TODO obtener validacion desde un servicio.
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 }

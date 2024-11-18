2f5dfe198eb70f9921353e78baf9ed0d2e9e87ec
jjmuesesq <jjmuesesq@unal.edu.co>
2024-11-17 21:33:32 -0500

    :sparkles: Elaborar el validador de correo

--- src/app/shared/validators/email-validator.service.ts
+++ src/app/shared/validators/email-validator.service.ts
@@ -7,6 +7,25 @@ export class EmailValidator implements AsyncValidator{
 
   validate( control: AbstractControl ): Observable<ValidationErrors | null> {
     const email = control.value;
+    const httpCallObservable = new Observable<ValidationErrors | null>((subscriber) => {
+      console.log({ email });
+      if(email === 'jjmuesesq@unal.edu.co'){
+        subscriber.next({ emailTaken : true });
+        subscriber.complete();
+        //return;
+      }
+      subscriber.next(null);
+      subscriber.complete();
+    }).pipe(
+      delay(3000)
+    );
+
+    return httpCallObservable;
+  }
+
+  /*
+  validate( control: AbstractControl ): Observable<ValidationErrors | null> {
+    const email = control.value;
     console.log({ email });
     return of({
       emailTaken: true
@@ -14,5 +33,16 @@ export class EmailValidator implements AsyncValidator{
       delay(2000)
     )
   }
+
+  return this.http.get<any[](´http://localhost:3000/users?q=${email}´)
+    .pipe(
+        map( resp => {
+          return (resp.length === 0)
+            ? null
+            : { emailTaken: true }
+        }
+        )
+    )
+  */
 
 }
